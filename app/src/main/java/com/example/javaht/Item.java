e4d02eb1606b70ab03694f1b2a98e4cb1a84ed5e
LatexGlovesLmao <106094206+Lauri-Heiskanen@users.noreply.github.com>
2023-04-25 16:30:19 +0300

    added attribute name

--- app/src/main/java/com/example/javaht/Item.java
+++ app/src/main/java/com/example/javaht/Item.java
@@ -3,12 +3,18 @@ package com.example.javaht;
 import java.util.ArrayList;
 
 public class Item {
+    private String name;
     private String slotType; // what kind of slot this item can be equipped into. See ItemSlot.java for more info
     private ArrayList<StatChange> effects;
 
-    public Item(String slotType, ArrayList<StatChange> effects) {
+    public Item(String name, String slotType, ArrayList<StatChange> effects) {
+        this.name = name;
         this.slotType = slotType;
         this.effects = effects;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getSlotType() {
