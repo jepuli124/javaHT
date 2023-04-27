d6483fe8207d4715d361d26ccff9fea91d8b62b1
jepuli124 <konsta.jalkanen.kj@gmail.com>
2023-04-27 16:30:38 +0300

    Updated AfterBattleActivity to receive information in form of bundle from battle activity
    Currently in bundle are name, victories and did character won last match

--- app/src/main/java/com/example/javaht/AfterBattleActivity.java
+++ app/src/main/java/com/example/javaht/AfterBattleActivity.java
@@ -3,10 +3,13 @@ package com.example.javaht;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AfterBattleActivity extends AppCompatActivity {
+
+    TextView characterName, characterVictories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -15,17 +18,30 @@ public class AfterBattleActivity extends AppCompatActivity {
         int result = bundle.getInt("result");
         if (result == 1) {
             setContentView(R.layout.victory_screen);
+            characterName = findViewById(R.id.txtCharacterNameVic);
+            characterVictories = findViewById(R.id.txtCharacterVictories);
         } else {
             setContentView(R.layout.defeat_screen);
+            characterName = findViewById(R.id.txtCharacterNameDef);
+            characterVictories = findViewById(R.id.txtCharacterVictoriesDef);
         }
+        characterName.setText(bundle.getString("name"));
+        characterVictories.setText(bundle.getInt("victories"));
     }
 
-    
+
 
 
     public void backToCharacterListActivity(View view){
         Intent intent = new Intent(AfterBattleActivity.this, CharacterListActivity.class);
         startActivity(intent);
     }
+
+    public void backToFight(View view){
+        Intent intent = new Intent(AfterBattleActivity.this, BattleActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
--- app/src/main/java/com/example/javaht/BattleActivity.java
+++ app/src/main/java/com/example/javaht/BattleActivity.java
@@ -24,24 +24,30 @@ public class BattleActivity extends AppCompatActivity {
 
     public void attack(View view){
         if(battle.attack(battle.getPlayerCharacter(), battle.getEnemyCharacter()) == 1){
+            String name = CharacterStorage.getInstance().getMainFighter().getName();
+            int victories = CharacterStorage.getInstance().getMainFighter().getBattlesWon() + 1;
             battle.endBattle(1, this);
-            endActivity(1);
+            endActivity(1, name, victories);
         }
     }
 
     public void enemyAction(){
         if(battle.attack(battle.getEnemyCharacter(), battle.getPlayerCharacter()) == 1){
+            String name = CharacterStorage.getInstance().getMainFighter().getName();
+            int victories = CharacterStorage.getInstance().getMainFighter().getBattlesWon();
             battle.endBattle(0, this);
-            endActivity(0);
+            endActivity(0, name, victories);
         }
     }
 
 
 
-    public void endActivity(int result){
+    public void endActivity(int result, String name, int victories){
         Intent intent = new Intent(BattleActivity.this, CharacterListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putInt("result", result);
+        bundle.putInt("victories", victories);
+        bundle.putString("name", name);
         intent.putExtras(bundle);
         startActivity(intent);
     }
