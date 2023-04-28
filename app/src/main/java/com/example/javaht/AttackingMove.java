99e80daad089f0d023c5a3d7462ff8559ad8d25d
LatexGlovesLmao <106094206+Lauri-Heiskanen@users.noreply.github.com>
2023-04-28 03:20:36 +0300

    added name

--- app/src/main/java/com/example/javaht/AttackingMove.java
+++ app/src/main/java/com/example/javaht/AttackingMove.java
@@ -1,11 +1,17 @@
 package com.example.javaht;
 
 public class AttackingMove {
+    private String name;
     private int attackPower, hitChance;
 
-    public AttackingMove(int attackPower, int hitChance) {
+    public AttackingMove(String name, int attackPower, int hitChance) {
+        this.name = name;
         this.attackPower = attackPower;
         this.hitChance = hitChance;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getAttackPower() {
