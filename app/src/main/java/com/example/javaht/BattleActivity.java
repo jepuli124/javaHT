58d79fc7940ef44148b0b18d16c35e35446b8b8c
TuomasLattila <tuomas.lattila@outlook.com>
2023-04-28 14:01:07 +0300

    Made animations.

--- app/src/main/java/com/example/javaht/BattleActivity.java
+++ app/src/main/java/com/example/javaht/BattleActivity.java
@@ -3,6 +3,9 @@ package com.example.javaht;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,36 +13,48 @@ import androidx.appcompat.app.AppCompatActivity;
 public class BattleActivity extends AppCompatActivity {
 
     private Battle battle;
-    TextView battleTextView = (TextView) findViewById(R.id.textViewBatlefield);
+    private Animation animation;
+    private ImageView playerImg, enemyImg;
+    //TextView battleTextView = (TextView) findViewById(R.id.textViewBatlefield);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fight_page);
+
+        playerImg = findViewById(R.id.imageViewPlayer);
+        enemyImg = findViewById(R.id.imageViewEnemy);
+
         if (CharacterStorage.getInstance().getEnemyFighter() == null) {
             battle = new Battle(CharacterStorage.getInstance().getMainFighter());
         } else {
             battle = new Battle(CharacterStorage.getInstance().getMainFighter(), CharacterStorage.getInstance().getEnemyFighter());
         }
-        battleTextView.setText(battle.getBattleText());
+        //battleTextView.setText(battle.getBattleText());
     }
 
-    public void doQuickAttack(){
+    public void doQuickAttack(View view){
         // can be called with onClick
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.quick_player);
+        playerImg.startAnimation(animation);
         battle.doQuickAttack(battle.getPlayerCharacter(), battle.getEnemyCharacter()); // comparison value 200
         afterPlayerAttack();
     }
 
-    public void doMediumAttack(){
+    public void doMediumAttack(View view){
         // can be called with onClick
-        battle.doMediumAttack(battle.getPlayerCharacter(), battle.getEnemyCharacter()); // comparison value 240
-        afterPlayerAttack();
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.medium_player);
+        playerImg.startAnimation(animation);
+        //battle.doMediumAttack(battle.getPlayerCharacter(), battle.getEnemyCharacter()); // comparison value 240
+        //afterPlayerAttack();
     }
 
-    public void doHeavyAttack(){
+    public void doHeavyAttack(View view){
         // can be called with onClick
-        battle.doHeavyAttack(battle.getPlayerCharacter(), battle.getEnemyCharacter()); // comparison value 280
-        afterPlayerAttack();
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.heavy_player);
+        playerImg.startAnimation(animation);
+        //battle.doHeavyAttack(battle.getPlayerCharacter(), battle.getEnemyCharacter()); // comparison value 280
+        //afterPlayerAttack();
     }
 
     private void afterPlayerAttack(){
@@ -47,19 +62,19 @@ public class BattleActivity extends AppCompatActivity {
         int battleStatusPreAi = battle.checkIfBattleEnded();
         if (battleStatusPreAi == 1 ||battleStatusPreAi == 2 || battleStatusPreAi == 3) {
             battle.endBattle(battleStatusPreAi, this);
-            battleTextView.setText(battle.getBattleText());
+            //battleTextView.setText(battle.getBattleText());
             endActivity(battleStatusPreAi, battle.getOriginalPlayerCharacterName(), battle.getOriginalPlayerCharacterVictories());
         }
-        battleTextView.setText(battle.getBattleText());
+        //battleTextView.setText(battle.getBattleText());
         if (battleStatusPreAi == 0) {
             battle.doAiAction(battle.getEnemyCharacter(), battle.getPlayerCharacter());
             int battleStatusPostAi = battle.checkIfBattleEnded();
             if (battleStatusPostAi == 1 ||battleStatusPostAi == 2 || battleStatusPostAi == 3) {
                 battle.endBattle(battleStatusPostAi, this);
-                battleTextView.setText(battle.getBattleText());
+                //battleTextView.setText(battle.getBattleText());
                 endActivity(battleStatusPostAi, battle.getOriginalPlayerCharacterName(), battle.getOriginalPlayerCharacterVictories());
             }
-            battleTextView.setText(battle.getBattleText());
+            //battleTextView.setText(battle.getBattleText());
         }
     }
 
